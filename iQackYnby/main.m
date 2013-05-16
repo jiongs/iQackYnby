388e7162ae03ca67a24c6aca937c788807f9c725
kmetop <119588911@qq.com>
2013-05-16 09:04:51 +0800

    Initial Commit

---
+++ iQackYnby/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iQackYnby
+//
+//  Created by ji yongshan on 13-5-16.
+//  Copyright (c) 2013年 ji yongshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QackAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QackAppDelegate class]));
+    }
+}
